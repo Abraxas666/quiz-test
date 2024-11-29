db7d94ac44b93747f800bfa8f5f6ef166961a7cb
Abraxas666 <aghosca@gmail.com>
2024-11-29 19:31:48 +0100

    Aggiunto il programma main

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+
+selection = int(input(""" 
+                *****************  PYTHON QUIZ V 1.0  *************************** 
+                *                                                               * 
+                *  Digitare 1 per avviare una prova di test                     * 
+                *  Digitare 2 to be defined                                     * 
+                *  Digitare 3 to be defined                                     * 
+                *                                                               * 
+                *****************************************************************\n
+                """
+                ))
+
+if selection == 1:
+    print ( "Qui inseriremo la funzione che richiama il test")
+if selection == 2:
+    print ( "Qui inseriremo la funzione che consente di aggiungere altre domande")
+if selection == 3:                            
+   print ( " Qui inseriremo una funzione che stampa un test cartaceo")
