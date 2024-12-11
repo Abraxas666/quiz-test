b6b21fa9c30a1cb2d3a346f5517dedd140636e33
AlinaK1997 <alinakompanets27@gmail.com>
2024-12-11 21:32:09 +0100

    I worked on the code that allows printing questions and answer options, taking the student's answer, comparing it to the right answer, showing whether the answwer is correct and  showing the correct answer if the wrong answer was inserted. At the end, the number of correctly answered questions is shown against the total number of questions.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
-
+import FlavioFx
 
 selection = int(input(""" 
                 *****************  PYTHON QUIZ V 1.0  *************************** 
                 *                                                               * 
                 *  Digitare 1 per avviare una prova di test                     * 
-                *  Digitare 2 to be defined                                     * 
-                *  Digitare 3 to be defined                                     * 
+                *  Digitare 2 per aggiunger altre domande                       * 
+                *  Digitare 3 per stampare un test cartaceo                     * 
                 *                                                               * 
                 *****************************************************************\n
                 """
@@ -14,7 +14,42 @@ selection = int(input("""
 
 if selection == 1:
     print ( "Qui inseriremo la funzione che richiama il test")
-if selection == 2:
-    print ( "Qui inseriremo la funzione che consente di aggiungere altre domande")
-if selection == 3:                            
-   print ( " Qui inseriremo una funzione che stampa un test cartaceo")
+
+questions = [
+    {
+        "question": "Come si chiamano i simboli < > = in python?",
+        "choices": ["1. Operatori di confronto", "2. Operatori logici", "3. Metodi di confronto"],
+        "answer": "1"
+    },
+    {
+        "question": "Che operatori si usano per il controllo di flusso?",
+        "choices": ["1. if, ifelse, while", "2. for, while", "3. try except"],
+        "answer": "1"
+    },
+    {
+        "question": "Nella definizione di funzione cosa viene contenuto tra le parentesi?",
+        "choices": ["1. gli argomenti", "2.  i parametri", "3. le stringhe"],
+        "answer": "2"
+    }
+]
+
+def run_quiz(questions):
+    score = 0
+    for q in questions:
+        print(q["question"])
+        for choice in q["choices"]:
+            print(choice)
+        answer = input("Enter your answer:")
+        if answer == q["answer"]:
+            print("Correct!\n")
+            score += 1
+        else:
+            print(f"Wrong! The correct answer is {q['answer']}\n")
+    print(f"Your final score is {score}/{len(questions)}")
+
+run_quiz(questions)
+
+# if selection == 2:
+#     print ( "Qui inseriremo la funzione che consente di aggiungere altre domande")
+# if selection == 3:                            
+#    print ( " Qui inseriremo una funzione che stampa un test cartaceo")
